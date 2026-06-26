23012a5189847e845facef02d21fe70070238eba
wmoreside <will.moreside@proton.me>
2026-06-25 23:14:37 -0400

    Add defaults

--- scripts/SmoothScaling/core/settings.lua
+++ scripts/SmoothScaling/core/settings.lua
@@ -14,27 +14,27 @@ local weaponScalingSettings = storage.playerSection("SettingsPlayerSmoothScaling
 local debugSettings = storage.playerSection("SettingsPlayerSmoothScalingDebug")
 
 M.getGlobalFrom = function()
-    return globalSettings:get("globalFrom")
+    return globalSettings:get("globalFrom") or 100
 end
 
 M.getGlobalTo = function()
-    return globalSettings:get("globalTo")
+    return globalSettings:get("globalTo") or 100
 end
 
 M.getMajorMultiplier = function()
-    return classSkillSettings:get("majorMultiplier")
+    return classSkillSettings:get("majorMultiplier") or 100
 end
 
 M.getMinorMultiplier = function()
-    return classSkillSettings:get("minorMultiplier")
+    return classSkillSettings:get("minorMultiplier") or 100
 end
 
 M.getMiscMultiplier = function()
-    return classSkillSettings:get("miscMultiplier")
+    return classSkillSettings:get("miscMultiplier") or 100
 end
 
 M.getSpecializationMultiplier = function()
-    return specializationSettings:get("specializationMultiplier")
+    return specializationSettings:get("specializationMultiplier") or 100
 end
 
 M.getIndividualMultiplier = function(skillId)
@@ -42,51 +42,51 @@ M.getIndividualMultiplier = function(skillId)
 end
 
 M.getSpellCostEnabled = function()
-    return spellCastSettings:get("spellCostEnabled")
+    return spellCastSettings:get("spellCostEnabled") or false
 end
 
 M.getXpPerMagicka = function()
-    return spellCastSettings:get("xpPerMagicka")
+    return spellCastSettings:get("xpPerMagicka") or 0.1
 end
 
 M.getArmorScalingEnabled = function()
-    return armorScalingSettings:get("armorScalingEnabled")
+    return armorScalingSettings:get("armorScalingEnabled") or false
 end
 
 M.getArmorScalingFrom = function()
-    return armorScalingSettings:get("armorScalingFrom")
+    return armorScalingSettings:get("armorScalingFrom") or 100
 end
 
 M.getArmorScalingTo = function()
-    return armorScalingSettings:get("armorScalingTo")
+    return armorScalingSettings:get("armorScalingTo") or 100
 end
 
 M.getBlockScalingEnabled = function()
-    return blockScalingSettings:get("blockScalingEnabled")
+    return blockScalingSettings:get("blockScalingEnabled") or false
 end
 
 M.getBlockScalingFrom = function()
-    return blockScalingSettings:get("blockScalingFrom")
+    return blockScalingSettings:get("blockScalingFrom") or 100
 end
 
 M.getBlockScalingTo = function()
-    return blockScalingSettings:get("blockScalingTo")
+    return blockScalingSettings:get("blockScalingTo") or 100
 end
 
 M.getWeaponScalingEnabled = function()
-    return weaponScalingSettings:get("weaponScalingEnabled")
+    return weaponScalingSettings:get("weaponScalingEnabled") or false
 end
 
 M.getWeaponScalingFrom = function()
-    return weaponScalingSettings:get("weaponScalingFrom")
+    return weaponScalingSettings:get("weaponScalingFrom") or 100
 end
 
 M.getWeaponScalingTo = function()
-    return weaponScalingSettings:get("weaponScalingTo")
+    return weaponScalingSettings:get("weaponScalingTo") or 100
 end
 
 M.getEnableMessages = function()
-    return debugSettings:get("enableMessages")
+    return debugSettings:get("enableMessages") or false
 end
 
 return M
